1f37838bf37fe21c672588f29fc6dd12089222bd
1966366130 <1966366130@qq.com>
2017-07-17 00:30:52 +0800

    爬取具体内容

--- dingdian/dingdian/spiders/dingdian.py
+++ dingdian/dingdian/spiders/dingdian.py
@@ -19,4 +19,31 @@ class Myspider(scrapy.Spider):
 		yield Request('http://www.x23us.com/quanben/1',self.parse)
 
 	def parse(self,response):
-		print(response.text)
+		max_num=BeautifulSoup(response.text,'lxml').find('div',class_='pagelink').find_all('a')[-1].get_text()
+		bashurl=str(response.url)[:-7]
+		for num in range(1,int(max_num)+1):
+			url=bashurl+'_'+str(num)+self.bashurl
+			yield Request(url,callback=self.get_name)
+
+	def get_name(self,response):
+		tds=BeautifulSoup(response.text,'lxml').find_all('tr',bgcolor='#FFFFFF')
+		for td in tds:
+			novelname=td.find('a').get_text()
+			novelurl=td.find('a')['href']
+			yield Request(novelurl,callback=self.get_chapterurl,meta={'name':novelname,'url':novelurl})
+
+	def get_chapterurl(self,response):
+		item=DingdianItem()
+		item['name']=str(response.meta['name']).replace('\xa0','')
+		item['novelurl']=response.meta['url']
+		category=BeautifulSoup(response.text,'lxml').find('table').find('a').get_text()
+		author=BeautifulSoup(response.text,'lxml').find('table').find_all('td')[2].get_text()
+		
+		serialstatus=BeautifulSoup(response.text).find('table').find_all('td')[5].get_text()
+		serialnumber=BeautifulSoup(response.text).find('table').find_all('td')[3].get_text()
+		item['category']=str(category).replace('/','')
+		item['author']=str(author).replace('/','')
+#		item['name_id']=name_id
+		item['serialstatus']=str(serialstatus).replace('/','')
+		item['serialnumber']=str(serialnumber).replace('/','')
+		return item
