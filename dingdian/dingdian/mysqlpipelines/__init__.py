a5a89026d3a5994240df7f2bfe73e00fb8a89f86
1966366130 <1966366130@qq.com>
2017-07-18 23:46:47 +0800

    SQL语句

---
+++ dingdian/dingdian/mysqlpipelines/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-07-18 22:54:17
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+import os
