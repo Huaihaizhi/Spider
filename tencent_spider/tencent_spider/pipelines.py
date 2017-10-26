735ae7098bf7ddc86ca233419a955292de8dd634
Huaihaizhi <1966366130@qq.com>
2017-10-26 17:49:06 +0800

    scrapy tencent

---
+++ tencent_spider/tencent_spider/pipelines.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import json
+
+class TencentSpiderPipeline(object):
+	def __init__(self):
+		self.filename=open('tencent.csv','w',encoding='utf-8')
+
+	def process_item(self,item,spider):
+
+		jsontext=item
+		self.filename.write(str(jsontext)+'\n')
+
+	def close_spider(self):
+		self.filename.close()
---
+++ tencent_spider/tencent_spider/spiders/tencentspider.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from tencent_spider.items import TencentSpiderItem
+
+
+
+class TencentspiderSpider(scrapy.Spider):
+    name = 'tencentspider'
+    allowed_domains = ['tencent.com']
+    url='http://hr.tencent.com/position.php?&start='
+    offset=0
+    start_urls = [url+str(offset)]
+
+    def parse(self, response):
+        for each in response.xpath('//tr[@class="odd"] | //tr[@class="even"]'):
+        	item=TencentSpiderItem()
+        	position_name=each.xpath('./td[1]/a/text()').extract()
+        	position_link=each.xpath('./td[1]/a/@href').extract()
+        	if each.xpath('./td[2]/text()').extract():
+        		position_cate=each.xpath('./td[2]/text()').extract()
+        	else:
+        		position_cate=['']
+        	position_number=each.xpath('./td[3]/text()').extract()
+        	position_location=each.xpath('./td[4]/text()').extract()
+        	position_time=each.xpath('./td[5]/text()').extract()
+
+        	item['position_name']=position_name[0]
+        	item['position_link']=position_link[0]
+        	item['position_cate']=position_cate[0]
+        	item['position_number']=position_cate[0]
+        	item['position_location']=position_location[0]
+        	item['position_time']=position_location[0]
+
+        	yield item
+
+        if self.offset<2370:
+        	self.offset+=10
+        yield scrapy.Request(self.url+str(self.offset),callback=self.parse)
